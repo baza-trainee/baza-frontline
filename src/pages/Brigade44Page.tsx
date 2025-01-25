631d9718a798124814214e948a4cf1b3116692f4
cold <kholodihor@gmail.com>
2025-01-25 10:02:09 +0200

    perf: improve share section

--- src/pages/Brigade44Page.tsx
+++ src/pages/Brigade44Page.tsx
@@ -1,12 +1,37 @@
 import ShareSection from '@/components/brigades/ShareSection';
-import DonationForm from '@/components/brigades/donation-form/DonationForm';
+import SEO from '@/components/common/SEO';
 
 const Brigade44Page = () => {
+  const currentDate = new Date().toISOString();
+
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center'>
-      <DonationForm bankType='monobank' />
-      <ShareSection title='Підтримай 44-ту окрему бригаду' hashtag='#44Brigade' />
-    </div>
+    <>
+      <SEO
+        title='44 ОАБр - Підтримка бригади | Baza Frontline'
+        description='Підтримайте 44-ту окрему артилерійську бригаду ЗСУ. Ваша допомога наближає перемогу України!'
+        url='/44'
+        type='website'
+        keywords={[
+          '44 ОАБр',
+          '44 бригада',
+          'підтримка військових',
+          'допомога армії',
+          'артилерійська бригада',
+          'волонтерська допомога',
+          'підтримка ЗСУ',
+          'донати військовим',
+          'допомога фронту',
+          'Україна',
+        ]}
+        language='uk'
+        modifiedTime={currentDate}
+        noindex={false}
+      />
+      <div className='flex min-h-screen flex-col items-center justify-center'>
+        {/* <DonationForm bankType='monobank' /> */}
+        <ShareSection title='Підтримай 44-ту окрему бригаду' />
+      </div>
+    </>
   );
 };
 
